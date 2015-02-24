4234705c59a7120bf954e5bfe9c41af5b7f912e8
Hilko Bengen <bengen@hilluzination.de>
2015-02-24 22:38:06 +0100

    use NAMESPACE in go-yara_URL

--- package/go-yara.mk
+++ package/go-yara.mk
@@ -1,7 +1,7 @@
 go-yara_VERSION     := 2786608186b745d0dc67a04a54c6d0e55cd68fdc
-go-yara_URL         := https://github.com/hillu/go-yara/archive/$(go-yara_VERSION).tar.gz
+go-yara_NAMESPACE   := github.com/hillu/go-yara
+go-yara_URL         := https://$(go-yara_NAMESPACE)/archive/$(go-yara_VERSION).tar.gz
 go-yara_BUILDSYSTEM := go
 go-yara_SUFFIX      := .tar.gz
-go-yara_NAMESPACE   := github.com/hillu/go-yara
 # go-yara_BUILDFLAGS  := 
 go-yara_DEPENDS     := yara
