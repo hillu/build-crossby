d2e2f68b39fba78dd5fd8e8d2a281f4df03495cd
Hilko Bengen <bengen@hilluzination.de>
2015-05-27 21:33:41 +0200

    Add go-bindata as example for executables

---
+++ package/go-bindata.mk
@@ -0,0 +1,6 @@
+go-bindata_VERSION     := 3.0.7
+go-bindata_NAMESPACE   := github.com/jteeuwen/go-bindata
+go-bindata_URL         := https://$(go-bindata_NAMESPACE)/archive/v$(go-bindata_VERSION).tar.gz
+go-bindata_POSTUNPACK  := ./bootstrap.sh
+go-bindata_BUILDSYSTEM := go
+go-bindata_SUFFIX      := .tar.gz
