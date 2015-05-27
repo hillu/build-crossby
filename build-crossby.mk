b0f9e31f2dbbb4ccb82ca426466419dccae85b6d
Hilko Bengen <bengen@hilluzination.de>
2015-05-27 21:33:10 +0200

    go buildsystem: Install executables

--- build-crossby.mk
+++ build-crossby.mk
@@ -205,6 +205,14 @@ define go_INSTALL
 $(BC_ROOT)/build/$(1)/$(2)/.install-stamp:
 	mkdir -p $(BC_ROOT)/target/lib/go
 	cp -urt $(BC_ROOT)/target/lib/go/ $(BC_ROOT)/build/$(1)/$(2)/pkg $(BC_ROOT)/build/$(1)/$(2)/src
+	mkdir -p $(BC_ROOT)/target/bin/$(2)
+# FIXME: Add a function to filter-out filenames
+	$$(foreach binary,\
+		$$(wildcard $(BC_ROOT)/build/$(1)/$(2)/bin/* \
+			$(BC_ROOT)/build/$(1)/$(2)/bin/$(call GOOS,$(2))_$(call GOARCH,$(2)_)/*),\
+		if test -f $$(binary); then \
+			install -m755 $$(binary) $(BC_ROOT)/target/bin/$(2);\
+		fi)
 	touch $$@
 # END go_INSTALL PACKAGE=$(1) ARCH=$(2)
 endef
