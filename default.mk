19fe66578e9359f46c273b419a1f5c06a397fadf
Hilko Bengen <bengen@hilluzination.de>
2015-07-05 19:11:50 +0200

    Add musl

--- default.mk
+++ default.mk
@@ -1,2 +1,2 @@
 BC_ARCHS    = x86_64-linux-gnu i386-linux-gnu i686-w64-mingw32 x86_64-w64-mingw32
-BC_PACKAGES = yara go-yara go-ole go-bindata
+BC_PACKAGES = musl yara go-yara go-ole go-bindata
---
+++ package/musl.mk
@@ -0,0 +1,25 @@
+musl_VERSION     := 1.1.10
+musl_URL         := http://www.musl-libc.org/releases/musl-$(musl_VERSION).tar.gz
+
+musl_BUILDSYSTEM := autoconf
+
+musl_x86_64-linux-musl_BUILDFLAGS := --target=x86_64
+musl_i386-linux-musl_BUILDFLAGS   := --target=i386
+
+musl_SUFFIX := .tar.gz
+musl_ARCHS  := i386-linux-musl x86_64-linux-musl
+
+# Patch the .specs file so we can can build both i386 and x86_64
+# binaries, provided we have a multilib-enabled GCC.
+musl_x86_64-linux-musl_POSTINSTALL := \
+	ln -s $(BC_ROOT)/target/bin/x86_64-linux-musl/musl-gcc \
+		$(BC_ROOT)/target/bin/x86_64-linux-musl-gcc && \
+	sed -i -e 's/^-dynamic-linker/-m elf_x86_64 &/' \
+		-e '$$$$a *multilib:\n64:../lib64:x86_64-linux-gnu m64;\n\n*multilib_defaults:\nm64\n\n*asm:\n--64\n\n*cc1_cpu:\n-m64' \
+		$(BC_ROOT)/target/lib/x86_64-linux-musl/musl-gcc.specs
+musl_i386-linux-musl_POSTINSTALL := \
+	ln -s $(BC_ROOT)/target/bin/i386-linux-musl/musl-gcc \
+		$(BC_ROOT)/target/bin/i386-linux-musl-gcc && \
+	sed -i -e 's/^-dynamic-linker/-m elf_i386 &/' \
+		-e '$$$$a *multilib:\n32:../lib32:i386-linux-gnu m32;\n\n*multilib_defaults:\nm32\n\n*asm:\n--32\n\n*cc1_cpu:\n-m32' \
+		$(BC_ROOT)/target/lib/i386-linux-musl/musl-gcc.specs
