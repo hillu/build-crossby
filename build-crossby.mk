c2d47c4913aa0611b69ad554edf38d987a241583
Hilko Bengen <bengen@hilluzination.de>
2016-09-10 17:10:30 +0200

    Update Copyright notice in makefile

--- build-crossby.mk
+++ build-crossby.mk
@@ -1,6 +1,6 @@
 # build-crossby.
 #
-# Copyright (C) 2015  Hilko Bengen
+# Copyright (C) 2015, 2016  Hilko Bengen <bengen@hilluzination.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
